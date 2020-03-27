0819f71b2273e10255aaf433d13c3ce82af0b732
Elaine Wang <wangelaine@berkeley.edu>
2020-03-27 00:08:53 -0700

    useraggregator

---
+++ main/UserAggregator/config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# counts during each of the time period
+user_activity_types = {
+    'active': 5,
+    'semi-active': 3,
+    'rare': 1,
+    'not active': 0
+}
+
+user_segmentations = ["age", "gender", "age and gender"]
+
+user_identity = user_segmentations[0]
