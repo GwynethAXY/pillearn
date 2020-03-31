111dc9a28a60101307b15dedfff5266e1b3021d8
hanzhesit <sith0008@e.ntu.edu.sg>
2020-03-30 21:13:24 -0700

    SP20PIL-14

--- main/CohortAnalysisPipeline/UsageCounter/UsageCounter.py
+++ main/CohortAnalysisPipeline/UsageCounter/UsageCounter.py
@@ -1,7 +1,3 @@
-import sys
-import pyspark as ps
-import warnings
-import re
 import json
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
@@ -43,16 +39,14 @@ def percentageActive(numActive,numTotal):
 def durationCheck(duration,length):
     return not (int(duration)>int(length) or int(duration)<0)
 
-def main(inputDir, contentMap, outputDir, configFile):
-    LONG_ITEM_DURATION_THRESHOLD = int(configFile.get('usage','LONG_ITEM_DURATION_THRESHOLD'))
-    LONG_ITEM_PERCENTAGE_THRESHOLD = float(configFile.get('usage','LONG_ITEM_PERCENTAGE_THRESHOLD'))
-
+def main(inputDir, contentMap, outputDir, config):
+    LONG_ITEM_DURATION_THRESHOLD = int(config['usage']['LONG_ITEM_DURATION_THRESHOLD'])
+    LONG_ITEM_ACTIVE_PERCENTAGE = float(config['usage']['LONG_ITEM_ACTIVE_PERCENTAGE'])
     duration_udf = udf(lambda start, end: getDuration(start,end))
     length_udf = udf(lambda length: convertLength(length))
     count_active_udf = udf(lambda duration,length, durationThreshold, percentageThreshold: countForActive(duration,length, durationThreshold, percentageThreshold))
     percentage_udf = udf(lambda numActive, numTotal: percentageActive(numActive,numTotal))
     duration_check_udf = udf(lambda duration, length: durationCheck(duration,length))
-    
     content_map = spark.read.csv(contentMap,header='true').select('title','length')
     df = spark.read.parquet(inputDir+'/*').dropDuplicates().na.drop()
     df = df.join(content_map,[df.item_name == content_map.title])
@@ -60,7 +54,7 @@ def main(inputDir, contentMap, outputDir, configFile):
     df = df.withColumn('length', length_udf(df['length']))
     df = df.withColumn('check',duration_check_udf(df['duration'],df['length']))
     df = df.filter(df['check']=='true').drop('check')
-    df = df.withColumn('counts_for_active', count_active_udf(df['duration'],df['length'],lit(LONG_ITEM_DURATION_THRESHOLD,lit(LONG_ITEM_PERCENTAGE_THRESHOLD))))
+    df = df.withColumn('counts_for_active', count_active_udf(df['duration'],df['length'],lit(LONG_ITEM_DURATION_THRESHOLD),lit(LONG_ITEM_ACTIVE_PERCENTAGE)))
     df = df.groupBy('device_id','bucket').agg(F.sum('counts_for_active').alias('count'))
     df.write.parquet(outputDir)
 
@@ -71,6 +65,6 @@ if __name__ == "__main__":
     parser.add_argument('-o', '--output', required=True)
     parser.add_argument('-c', '--config', required=True)
     args = parser.parse_args()
-    config = ConfigParser()
-    config.read(args.config)
+    with open('config.json') as f:
+        config = json.load(f)
     main(args.input,args.map,args.output,config)
