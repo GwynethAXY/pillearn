e7a18ae3d5ec9825505c30cc28fe84733f23816b
Elaine Wang <wangelaine@berkeley.edu>
2020-04-03 03:05:01 -0700

    log4j warnings

--- main/SubscriptionContentPipeline/PopularityAggregator/PopularityAggregator.py
+++ main/SubscriptionContentPipeline/PopularityAggregator/PopularityAggregator.py
@@ -1,12 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
 ### common pyspark import statements
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.context import SQLContext
-from pyspark.sql.functions import udf 
+from pyspark.sql.functions import udf
 from pyspark.sql.functions import lit
 from pyspark.sql.window import Window
 import pyspark.sql.functions as F
-import pyspark.sql.types as T 
+import pyspark.sql.types as T
 from pyspark.sql.functions import split, explode
 ### other essnetial import statements
 import argparse
@@ -19,42 +24,38 @@ from datetime import datetime
 		- user defined functions
 		- main function
 		- program start point
-''' 
+'''
 
 # setup
 conf = SparkConf()
 conf.setAppName('pillar')
-sc = SparkContext(conf=conf)
+# sc = SparkContext(conf=conf)
+sc = SparkContext.getOrCreate(conf = conf);
+spark = SparkSession(sc)
 sc.setLogLevel('WARN')
 sql_context = SQLContext(sc)
 spark = SparkSession.builder.appName('pillar').getOrCreate()
-
-# user defined functions
-
-def userDefinedFunction1():
-	return
-
-def userDefinedFunction2():
-	return
-
 # main function: should always take in input file, output file and config file
-def main(inputFile, outputFile, configFIle):
-	return
+def main(inputDir,profileMap,outputDir,config):
+    profile = spark.read.csv(profileMap, header='true')
+    df = spark.read.parquet(inputDir+"/*")
+    df = df.join(profile, 'device_id')
+    df = df.select('item_name', 'weight', 'age', 'gender')
+    df = df.groupBy(['item_name', 'age', 'gender']).sum().alias('weighted_sum')
+    df.write.parquet(outputDir)
 
 # program start point
 if __name__ == "__main__":
-	''' 
+	'''
 		Add arguments to script during execution on command line
 		Example of how to run the script: spark-submit template.py -i input.parquet -o output.parquet -c config.ini
 	'''
-	parser = argparse.ArgumentParser() 
-	parser.add_argument('-i', '--input', required=True)
-	parser.add_argument('-o', '--output', required=True)
-	parser.add_argument('-c', '--config', required=True)
-	args = parser.parse_args()
-	config = ConfigParser()
-	config.read(args.config)
-	main(args.input,args.output,config)
-
-
-
+	parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', required=True)
+    parser.add_argument('-m', '--map', required=True)
+    parser.add_argument('-o', '--output', required=True)
+    parser.add_argument('-c', '--config', required=True)
+    args = parser.parse_args()
+    config = ConfigParser()
+    config.read(args.config)
+    main(args.input,args.map,args.output,config)
